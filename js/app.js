25fb86a787c56e963739c606dbc29d94931e6952
Niveen  AL Smadi <50866072+NiveenAlSmadi@users.noreply.github.com>
2021-03-01 21:03:40 +0200

    to make it more interactive

--- js/app.js
+++ js/app.js
@@ -14,6 +14,22 @@ let myName=prompt('so CAN you guess who is  the best future developer , who made
     {alert('just try it dear!!');}
        else alert ('Accept  yes/y or no/n only!! ') 
 
+
+
+//q1
+let location=prompt('so Do I am from france ?');
+
+
+ if(location.toLocaleLowerCase()==='yes' || myName.toLocaleLowerCase()=== 'y')
+ { //console.log('incorrect');
+   alert('Erreur *--* mean wrong in English ');}
+ else if (myName.toLocaleLowerCase() ==='no'|| myName.toLocaleLowerCase() ==='n')
+    {//console.log('correct');
+      alert('correct , Merci beaucoup');}
+       else alert ('Accept  yes/y or no/n only!! ') 
+
+
+//q2
 let age =prompt('so guess my age , is my age 25??')
 
 if(age.toLocaleLowerCase()==='yes' || age.toLocaleLowerCase()==='y')
@@ -26,7 +42,7 @@ if(age.toLocaleLowerCase()==='yes' || age.toLocaleLowerCase()==='y')
 
 
 
-
+//q3
 let phase =prompt('Am I student ??');
 if(phase.toLocaleLowerCase()==='yes'  || phase.toLocaleLowerCase()==='y')
  { //console.log('correct');
@@ -36,7 +52,7 @@ if(phase.toLocaleLowerCase()==='yes'  || phase.toLocaleLowerCase()==='y')
      alert('that is wrong  '+ ' '+ userName);}
       else alert ('Accept yes/y or no/n only!! ');
 
-
+//q4
 let dark=prompt(' Am I afraid of the dark?');
 if(dark.toLocaleLowerCase()==='yes' || dark.toLocaleLowerCase()==='y')
  { //console.log('incorrect');
@@ -47,7 +63,7 @@ if(dark.toLocaleLowerCase()==='yes' || dark.toLocaleLowerCase()==='y')
       else alert ('Accept yes/y or no/n only !!');
 
 
-
+//q5
 let love = prompt('Do i love food??');
 if(love.toLocaleLowerCase()==='yes' || love.toLocaleLowerCase()==='y')
 {  //console.log('correct');
