b75a53ac734dff79d99b3f1d03a7328a0b77c4e0
NiveenALSmadi <niveensmadi33@gmail.com>
2021-03-01 18:35:52 +0200

    to make the page more interactive

---
+++ js/app.js
@@ -0,0 +1,71 @@
+'use strict'; 
+
+let userName=prompt('Hey, What is our BEAUTIFUL customer name?');
+  alert('we hope you are well  ' +  userName +'  Wellcom to FOODI FOOD');
+
+//console.log('coustmer name :',userName);
+
+let myName=prompt('so CAN you guess who is the best future developer , who made this webpage ?');
+
+
+ if(myName.toLocaleLowerCase()==='yes' || myName.toLocaleLowerCase()=== 'y')
+ { alert('There are those who have high confidence here');}
+ else if (myName.toLocaleLowerCase() ==='no'|| myName.toLocaleLowerCase() ==='n')
+    {alert('just try it dear!!');}
+       else alert ('Accept  yes/y or no/n only!! ') 
+
+let age =prompt('so guess my age , is my age 25??')
+
+if(age.toLocaleLowerCase()==='yes' || age.toLocaleLowerCase()==='y')
+ { //console.log('incorrect');
+     alert('NOooo, I am not old ');}
+ else if  (age .toLocaleLowerCase() ==='no'|| age.toLocaleLowerCase()==='n')
+    { //console.log('correct');
+        alert('correctttt, i think you know me *--* ');}
+        else alert ('Accept yes/y or no/n only!! ');
+
+
+
+
+let phase =prompt('Am I student ??');
+if(phase.toLocaleLowerCase()==='yes'  || phase.toLocaleLowerCase()==='y')
+ { //console.log('correct');
+     alert('perfect - you Know me well  ');}
+ else if  (phase .toLocaleLowerCase() ==='no'|| phase.toLocaleLowerCase()=== 'n')
+ { //console.log('incorrect');
+     alert('that is wrong  '+ ' '+ userName);}
+      else alert ('Accept yes/y or no/n only!! ');
+//console.log('coustmer entering about phase :',phase);
+
+
+let dark=prompt(' Am I afraid of the dark?');
+if(dark.toLocaleLowerCase()==='yes' || dark.toLocaleLowerCase()==='y')
+ { //console.log('incorrect');
+     alert('incorrect, i live in dark actually');}
+ else if  (dark.toLocaleLowerCase() ==='no'|| dark.oLocaleLowerCase()==='n'){
+      //console.log('correct');
+     alert('correct, you trust mee '); }
+      else alert ('Accept yes/y or no/n only !!');
+
+//console.log('coustmer entering about phase :',phase);
+
+let love = prompt('Do i love food??');
+if(love.toLocaleLowerCase()==='yes' || love.toLocaleLowerCase()==='y')
+{  //console.log('correct');
+    alert('yaaaa, Absolutly i love food ');}
+ else if  (love.toLocaleLowerCase() ==='no' || love.toLocaleLowerCase()==='n'){
+      //console.log('incorrect');
+    alert('nuppp, i loveee it');
+}
+else
+      alert ('Accept yes/y or no/n only !!');
+
+
+
+ alert ('good job' + '  ' +   userName  +'nice to meet you ');
+
+
+
+
+
+
